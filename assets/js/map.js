3b375692623b5af240934ccab5c4c5a438db3e25
A_Duo <herathrocker29@gmail.com>
2024-04-21 05:04:33 -0600

    Made legislator link open in new tab

--- assets/js/map.js
+++ assets/js/map.js
@@ -142,7 +142,7 @@ class Map {
         let DATA = this.DISTRICT_DATA[this.GetChamber()][districtNum]
 
         let content = '<h1>District ' + (districtNum + 1)+'</h1><img src="'+DATA["imgUrl"]+'" style="width:106px;height:144px;border-radius:1.5ch; border:3px solid #ccc">'
-        content += '<a href="' + (this.isSenate ? 'https://senate.utah.gov/sen/' : 'https://house.utleg.gov/rep/') + DATA['imgUrl'].match(/\/([^\/]*)\.[^\.]*$/)[1] +'/"><div class="fa-solid fa-user-tie" style="margin:1ch 0 2ch 0"></div> '+DATA["name"]+'</a>'
+        content += '<a href="' + (this.isSenate ? 'https://senate.utah.gov/sen/' : 'https://house.utleg.gov/rep/') + DATA['imgUrl'].match(/\/([^\/]*)\.[^\.]*$/)[1] +'/" target="_blank"><div class="fa-solid fa-user-tie" style="margin:1ch 0 2ch 0"></div> '+DATA["name"]+'</a>'
 
         content += '<div class="mid-lined" style="--color:#333;width:100%" style="margin:0"><b>Legislative Action</b></div>'
         // content += '<div style="border: 0 solid #666; border-left: 3px">'
