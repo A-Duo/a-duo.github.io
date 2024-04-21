6a2b54f68540fcd579f861e6f684ae80117a37ca
A_Duo <herathrocker29@gmail.com>
2024-04-21 04:03:40 -0600

    Made some improvements to the map page

---
+++ Misc Scripts/District Info/get_bill_info.py
@@ -0,0 +1,92 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+from pathlib import Path
+import json
+
+BILL_LIST = [
+    #2024
+    #'2024HB0253', #no vote
+    '2024/HB0257',
+    '2024/HB0261',
+    '2024/HB0303',
+    '2024/HB0316', #prettier formatting: https://le.utah.gov/~2024/bills/hbillenr/HB0316.pdf
+    # '2024HCR018', #supported
+    # '2024HB0527' #no vote
+
+    #2023
+    #'2023HB0132', #failed in committee
+    '2023/HB0329', #https://www.sltrib.com/artsliving/2022/12/08/scott-d-pierce-hbos-drag-show/  https://www.qsaltlake.com/news/2023/03/04/bill-in-response-to-st-george-drag-shows-not-heard-before-end-of-utah-legislative-session/
+    '2023/HB0463',
+    #'2023HB0464', #line 137
+    '2023/SB0016', #line 240
+    '2023/SB0093', #line 229
+    '2023/SB0100', #line 44
+
+    #2022
+    '2022/HB0011',
+    '2022S3/HB3001', #protections for hb11 enforcement
+    
+    #2021
+    '2021/HB0302' #https://attheu.utah.edu/facultystaff/statement-regarding-house-bill-302/ https://apnews.com/article/utah-bills-legislature-government-and-politics-242a4f69473dba80d8455d352a93eb41
+]
+
+def getLegislators(table):
+    return list(map(lambda x: int(x), re.findall('href=".*dist=(\d*)\'\)', str(table))))
+
+def scrapeData(url):
+    url = url.replace("&amp;", "&")
+    print(url)
+    soup = BeautifulSoup(requests.get("https://le.utah.gov" + url).content, 'html.parser').find("body")
+    
+    data = {'url': url}
+    
+    TABLES = soup.findAll("table")
+
+    data['status'] = re.search('<b>[^-]*[^-]*-[^-]*-\s([^\r\n]*)', str(soup)).group(1)
+    data['yea'] = getLegislators(TABLES[0])
+    data['nay'] = getLegislators(TABLES[1])
+    data['abs'] = getLegislators(TABLES[2])
+
+    return data
+    
+billData = []
+
+for BILL in BILL_LIST:
+    print('---------------------')
+    _billUrl = BILL.split('/')
+    url = f"https://le.utah.gov/~{_billUrl[0]}/bills/static/{_billUrl[1]}.html"
+    print(url)
+    billHtml = BeautifulSoup(requests.get(url).content, 'html.parser')
+
+    data = {'info': BILL}
+
+    data['title'] = re.search('class="heading">([^<]*)</h3>', str(billHtml.find(id='main-content'))).group(1)
+
+    status = str(billHtml.find(id='billStatus'))
+
+    # Think this is accurate, checks if the last action was done by the governor's office.
+    # If passage status is inaccurate then this is why.
+    data['passed'] = re.search('([^>]*)<[^<]*>[^>]*><[^>]*><[^>]*>(?:[^>]*<\/[^<>]*>[^<]*)*\Z', status).group(1) == 'Executive Branch - Lieutenant Governor'
+
+    items = re.findall('<a href="([^"]*house=[HS])">\d', status)
+
+    houseLink = None
+    senateLink = None
+    for item in items:
+        if (item[-1] == 'S'):
+            senateLink = item
+        else:
+            houseLink = item
+
+    if houseLink:
+        data["houseVote"] = scrapeData(houseLink)
+    if senateLink:
+        data["senateVote"] = scrapeData(senateLink)
+
+    billData.append(data)
+
+print(billData)
+
+with open (Path(__file__).parent / 'utah-bills.json', 'w') as f:
+    json.dump(billData, f)
--- assets/js/map.js
+++ assets/js/map.js
@@ -11,23 +11,61 @@ class Map {
     constructor(script) {
         this.MAP_CONTAINER = script.parentElement.querySelector("#map-bounding-box");
         this.INFO_CONTAINER = script.parentElement.querySelector("#map-info-panel");
-
+        
         this.HOUSE_BUTTON = script.parentElement.querySelector("#house-button");
         this.SENATE_BUTTON = script.parentElement.querySelector("#senate-button");
+
+        this.EMPTY_INFO_PANEL_CONTENT = this.INFO_CONTAINER.innerHTML;
         
         this.isSenate = false; // false for house || true for senate
+
         this.DISTRICT_DATA = null;
+        this.BILL_DATA = null
         this.HOUSE_MAP = null;
         this.SENATE_MAP = null;
+
         this.init()
     }
 
     async init() {
-        fetch("/assets/misc/utah-district-info.json").then(response => response.json()).then(response => {this.DISTRICT_DATA = response})
-        fetch("/assets/misc/utah-house-map.html").then(response => response.text()).then(response => {
-            this.HOUSE_MAP = response;
-            this.DrawMap()
+        fetch("/assets/misc/utah-district-info.json").then(response => response.json()).then(response => {
+            this.DISTRICT_DATA = response
+            fetch("/assets/misc/utah-bills.json").then(response => response.json()).then(data => {
+                this.BILL_DATA = data
+
+                let VOTE_TYPES = ['yea', 'nay', 'abs']
+                let CHAMBERS = ['house', 'senate']
+        
+                for (let chamber of CHAMBERS) {
+                    for (let rep of this.DISTRICT_DATA[chamber]) {
+                        for (let type of VOTE_TYPES) {
+                            rep[type] = []
+                        }
+                    }
+                }
+        
+                for (let i = 0; i < data.length; i++) {
+                    for (let chamber of CHAMBERS) {
+                        let voteData = data[i][chamber+'Vote']
+                        
+                        if (voteData == undefined) {
+                            continue
+                        }
+        
+                        for (let type of VOTE_TYPES) {
+                            for (let district of voteData[type]) {
+                                this.DISTRICT_DATA[chamber][district - 1][type].push(i)
+                            }
+                        }
+                    }
+                }
+                fetch("/assets/misc/utah-house-map.html").then(response => response.text()).then(response => {
+                    this.HOUSE_MAP = response;
+                    this.DrawMap();
+                })
+            })
         })
+
         fetch("/assets/misc/utah-senate-map.html").then(response => response.text()).then(response => {this.SENATE_MAP = response})
 
         this.HOUSE_BUTTON.addEventListener("click", (e) => {this.ChamberButtonClicked(false)})
@@ -44,21 +82,28 @@ class Map {
         let districts = this.MAP_CONTAINER.querySelector("#svg-districts").children;
 
         for (let i = 0; i < districts.length; i++) {
-            let percent = (i/(districts.length-1))*120;
-            // let percent = (Math.floor(Math.random() * districts.length)/(districts.length-1))*120;
-            districts[i].style.fill = "hsl("+percent+", 100%, 65%)";
+            let elm = districts[i]
+            let curDistrict = this.DISTRICT_DATA[this.GetChamber()][i]
 
-            districts[i].addEventListener("mouseenter", (e) => {
-                districts[i].style.fill="hsl("+percent+", 100%, 75%)";
-                // districts[i].style.filter="drop-shadow(1px 1px 5px #000)";
+            let approved = curDistrict['yea'].length
+            let total = approved + curDistrict['nay'].length
+            let percent = ((total-approved)/total)*120; // hue
+            // let percent = ((total-approved)/total)*100; // sat
+
+            let baseColor = "hsl("+percent+", 100%, 65%)";
+            // let baseColor = "hsl(" + (this.isSenate ? "335" : "190") + ", "+ percent +"%, 65%)";
+            elm.style.fill = baseColor
+
+            elm.addEventListener("mouseenter", (e) => {
+                elm.style.fill="hsl("+percent+", 100%, 75%)";
+                // elm.style.fill = "hsl(" + (this.isSenate ? "335" : "190") + ", "+ percent +"%, 75%)";
             })
 
-            districts[i].addEventListener("mouseout", (e) => {
-                districts[i].style.fill="hsl("+percent+", 100%, 65%)";
-                // districts[i].style.filter="";
+            elm.addEventListener("mouseout", (e) => {
+                elm.style.fill = baseColor
             })
 
-            districts[i].addEventListener("click", (e) => {
+            elm.addEventListener("click", (e) => {
                 this.UpdateInfoCard(i);
             })
         }
@@ -80,6 +125,7 @@ class Map {
         prev.classList.remove("active");
         
         this.DrawMap();
+        this.INFO_CONTAINER.innerHTML = this.EMPTY_INFO_PANEL_CONTENT;
     }
 
     ChamberButtonClicked(isSenateButton) {
@@ -93,7 +139,25 @@ class Map {
     }
 
     UpdateInfoCard(districtNum) {
-        this.INFO_CONTAINER.innerHTML = '<h1>District ' + (districtNum + 1)+'</h1><img src="'+this.DISTRICT_DATA[this.GetChamber()][districtNum]["imgUrl"]+'" style="width:106px;height:144px;"><p>'+this.DISTRICT_DATA[this.GetChamber()][districtNum]["name"]+'</p><p>Insert legislative action here.</p>';
+        let DATA = this.DISTRICT_DATA[this.GetChamber()][districtNum]
+
+        let content = '<h1>District ' + (districtNum + 1)+'</h1><img src="'+DATA["imgUrl"]+'" style="width:106px;height:144px;border-radius:1.5ch; border:3px solid #ccc">'
+        content += '<div><div class="fa-solid fa-user-tie" style="margin:1ch 0 2ch 0"></div> '+DATA["name"]+'</div>'
+
+        content += '<div class="mid-lined" style="--color:#333;width:100%" style="margin:0"><b>Legislative Action</b></div>'
+        // content += '<div style="border: 0 solid #666; border-left: 3px">'
+        content += '<div class="alternating-table"><div class="table-body">'
+        content += '<div class="table-row tooltip-hover"><div class="tooltip-anchor fa-solid fa-scale-unbalanced table-data" style="color:#2cb52c"><span class="tooltip-text">Opposed</span></div><div class="icon-table-row table-data" style="text-align:center">' + DATA['nay'].length + '</div></div>'
+        content += '<div class="table-row tooltip-hover tb-odd"><div class="tooltip-anchor fa-solid fa-scale-unbalanced-flip table-data" style="color:#d64242"><span class="tooltip-text">Supported</span></div><div class="icon-table-row table-data" style="text-align:center">' + DATA['yea'].length + '</div></div>'
+        content += '<div class="table-row tooltip-hover"><div class="tooltip-anchor fa-solid fa-scale-balanced table-data" style="color:#888"><span class="tooltip-text">Abstained</span></div><div class="icon-table-row table-data" style="text-align:center">' + DATA['abs'].length + '</div></div>'
+        content += '</div></div>'
+        // content += '<p>Supported</p><ul>'
+        // for (let id of DATA['yea']) {
+        //     content += '<li>' + this.BILL_DATA[id]['title'] + '</li>'
+        // }
+        // content += '</ul>'
+
+        this.INFO_CONTAINER.innerHTML = content
     }
 }
 
