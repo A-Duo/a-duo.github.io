7f423450db13bcd2ddb3ab38db82ea401c1a2d71
A_Duo <herathrocker29@gmail.com>
2024-04-22 05:12:46 -0600

    fixed some minor rendering issues

--- assets/js/map.js
+++ assets/js/map.js
@@ -90,7 +90,7 @@ class LegMap {
                 }
 
                 this.VOTE_CHART = new Chart(this.VOTE_CHART_CANVAS, {
-                    type: 'bar',
+                    type: 'line',
                     data: {
                         labels: [],
                         datasets: [{
