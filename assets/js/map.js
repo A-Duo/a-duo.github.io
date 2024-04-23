4f6b7cc64ef7eaf27283a4c560a203673320b021
A_Duo <herathrocker29@gmail.com>
2024-04-23 17:31:43 -0600

    Updated map legislator headshot size

--- assets/js/map.js
+++ assets/js/map.js
@@ -224,7 +224,7 @@ class LegMap {
     UpdateInfoCard(districtNum) {
         let DATA = this.DISTRICT_DATA[this.GetChamber()][districtNum]
 
-        let content = '<h1>District ' + (districtNum + 1)+'</h1><img src="/assets/images/legislators/'+DATA.id+'.jpg" style="width:106px;height:144px;border-radius:1.5ch; border:3px solid #ccc">'
+        let content = '<h1>District ' + (districtNum + 1)+'</h1><img src="/assets/images/legislators/'+DATA.id+'.jpg">'
         content += '<a href="' + (this.isSenate ? 'https://senate.utah.gov/sen/' : 'https://house.utleg.gov/rep/') + DATA.id + '/" target="_blank"><div class="fa-solid fa-user-tie" style="margin:1ch 0 2ch 0"></div> '+DATA["name"]+'</a>'
 
         content += '<div class="mid-lined" style="--color:#ccc;width:100%" style="margin:0"><b>Legislative Action</b></div>'
